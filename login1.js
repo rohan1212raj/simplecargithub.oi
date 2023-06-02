aeb3c6f00d77d12547cbef2a324ad1fef899c406
rohan1212raj <rrohanraj1212@gmail.com>
2023-06-02 18:58:13 +0530

    my cars

---
+++ login1.js
@@ -0,0 +1,8 @@
+var card = document.getElementById("card");
+
+function openRegister(){
+card.style.transform = "rotatey(-180deg)";
+}
+function openLogin(){
+card.style.transform = "rotatey(0deg)";
+}
